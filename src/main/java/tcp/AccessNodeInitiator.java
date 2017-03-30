78d7f5753a2e91a0751d3661e4ec59d96d11cff1
DanChirtoaca <chirtoaca.dan@gmail.com>
2017-03-30 20:50:51 +0200

    extended functionality to allow comments in config files

--- src/main/java/tcp/AccessNodeInitiator.java
+++ src/main/java/tcp/AccessNodeInitiator.java
@@ -10,13 +10,17 @@ public class AccessNodeInitiator {
     public static AccessNode initAccessNode(String filename) throws IOException {
         BufferedReader in = new BufferedReader(new FileReader(filename));
 
-        int port = Integer.parseInt(in.readLine());
-        int numConnections = Integer.parseInt(in.readLine());
-        int numWorkerNodes = Integer.parseInt(in.readLine());
+        String[] portLine = in.readLine().split(" ");
+        int port = Integer.parseInt(portLine[0]);
 
-        String rmiline = in.readLine();
-        String[] rmiSpec = rmiline.split(" ");
-        AccessNode accessNode = new AccessNode(port, numWorkerNodes,rmiSpec[0], Integer.parseInt(rmiSpec[1]));
+        String[] numConnectLine = in.readLine().split(" ");
+        int numConnections = Integer.parseInt(numConnectLine[0]);
+
+        String[] numWorkerNodesLine = in.readLine().split(" ");
+        int numWorkerNodes = Integer.parseInt(numWorkerNodesLine[0]);
+
+        String[] rmiLine = in.readLine().split(" ");
+        AccessNode accessNode = new AccessNode(port, numWorkerNodes, rmiLine[0], Integer.parseInt(rmiLine[1]));
 
         for (int i=0; i < numConnections; ++i) {
             String line = in.readLine();
